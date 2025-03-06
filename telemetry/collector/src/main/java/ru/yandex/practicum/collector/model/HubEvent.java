79cb866e72562cb0dc8d57a886f0541ac67854de
3agent999 <agentsmith999@yandex.ru>
2025-03-06 21:12:34 +0300

    Доработать HubEvent

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/HubEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/HubEvent.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.collector.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -7,6 +9,18 @@ import lombok.Setter;
 
 import java.time.Instant;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = DeviceAddedEvent.class, name = "DEVICE_ADDED"),
+        @JsonSubTypes.Type(value = DeviceRemovedEvent.class, name = "DEVICE_REMOVED"),
+        @JsonSubTypes.Type(value = ScenarioAddedEvent.class, name = "SCENARIO_ADDED"),
+        @JsonSubTypes.Type(value = ScenarioRemovedEvent.class, name = "SCENARIO_REMOVED")
+})
 @Getter
 @Setter
 public class HubEvent {
