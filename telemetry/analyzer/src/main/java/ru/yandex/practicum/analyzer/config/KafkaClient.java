832ba7a4cd7270c743c9fefc7ef0f7d36ccb8033
educational1234 <agentsmith999@yandex.ru>
2025-03-19 23:09:05 +0300

    Внести изменения в analyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/config/KafkaClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/config/KafkaClient.java
@@ -1,35 +1,28 @@
 package ru.yandex.practicum.analyzer.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import ru.yandex.practicum.analyzer.consumer.SensorsSnapshotDeserializer;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
-import java.util.Properties;
+import java.time.Duration;
 
-@Configuration
+@Component
+@RequiredArgsConstructor
 public class KafkaClient {
 
-    @Value("${kafka.bootstrap.servers}")
-    private String bootstrapServers;
+    private final KafkaConsumer<String, SensorsSnapshotAvro> kafkaConsumer;
 
-    @Value("${kafka.consumer.group}")
-    private String consumerGroup;
+    public ConsumerRecords<String, SensorsSnapshotAvro> poll() {
+        return kafkaConsumer.poll(Duration.ofMillis(1000));
+    }
 
-    @Bean
-    public KafkaConsumer<String, SensorsSnapshotAvro> kafkaConsumer() {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    public void commit() {
+        kafkaConsumer.commitAsync();
+    }
 
-        return new KafkaConsumer<>(props);
+    public void close() {
+        kafkaConsumer.close();
     }
 }
