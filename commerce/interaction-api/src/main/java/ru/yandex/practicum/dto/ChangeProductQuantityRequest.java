e709e1fc651c46b73bf9905056cb7268e4acb978
educational1234 <agentsmith999@yandex.ru>
2025-04-10 17:16:50 +0300

    Доработать dto

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/ChangeProductQuantityRequest.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/ChangeProductQuantityRequest.java
@@ -2,13 +2,17 @@ package ru.yandex.practicum.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangeProductQuantityRequest {
 
     @NotNull
