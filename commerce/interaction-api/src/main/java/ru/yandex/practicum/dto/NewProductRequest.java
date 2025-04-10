95d3cc467951e0a177b41c8a26097fa04976ecc6
educational1234 <agentsmith999@yandex.ru>
2025-04-10 14:04:53 +0300

    Доработать NewproductsRequest

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/NewProductRequest.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/NewProductRequest.java
@@ -4,32 +4,36 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewProductRequest {
-    @NotNull
+    @NotNull(message = "Идентификатор продукта обязателен")
     UUID productId;
 
-    @NotBlank
+    @NotBlank(message = "Название товара не может быть пустым")
     String productName;
 
-    @NotBlank
+    @NotBlank(message = "Описание товара не может быть пустым")
     String description;
 
     String imageSrc;
 
-    @NotNull
+    @NotNull(message = "Состояние остатка товара обязательно")
     QuantityState quantityState;
 
-    @NotNull
+    @NotNull(message = "Статус товара обязателен")
     ProductState productState;
 
-    @NotNull
+    @NotNull(message = "Категория товара обязательна")
     ProductCategory productCategory;
 
     @Min(value = 1, message = "Цена не может быть меньше 1")
